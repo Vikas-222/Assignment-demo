db1b429b485d938691e1faefb2735ededcf7e06f
vikas <vsa@narola.email>
2025-01-22 11:27:29 +0530

    updated

--- Assignment1/Exceptions/OrderNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package Exceptions;
-
-public class OrderNotFoundException extends Exception{
-
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
--- Assignment1/Exceptions/StockUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package Exceptions;
-
-public class StockUnavailableException extends Exception{
-
-        public StockUnavailableException(String error) {
-            super(error);
-        }
-}
