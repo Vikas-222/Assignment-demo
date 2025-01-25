9880fb79b534c23daf2cc9cc982b51f4f575d310
vikas <vsa@narola.email>
2025-01-25 13:33:22 +0530

    Records added

--- Records/RecordExample.java
+++ Records/RecordExample.java
@@ -2,6 +2,15 @@ package com.company.Records;
 
 public record RecordExample(String name,int rollno) {
 
+//    Record constructor behaviour is different from the constructors in normal classes.
+//    By default records have an all arguments constructor which is called canonical constructor.
+//    it is created by compiler if we don't create it then
+
+
+
+//    In fact the compact constructor isn’t an additional constructor, rather its container that contains
+//    some code for validations / handle the defensive copies of mutable objects and is being injected to the normal canonical constructor.
+//    This compact constructor doesn’t initialise the values.
     public RecordExample{
         if(name == ""){
             name = "Vikas";
